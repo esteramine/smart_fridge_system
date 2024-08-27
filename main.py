38e62452ce5229d88d24627437f69874e640fe89
Ann Chou <annchou@ibm.com>
2024-08-27 18:02:17 +0800

    [main] test db api

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import genai
+import database as db
 
-def create_food_item(label, left, top, right, bottom, score, expiration_date):
+def create_food_item(label, left, top, right, bottom, expiration_date):
     return {
         "label": label,
         "left": left,
         "top": top,
         "right": right,
         "bottom": bottom,
-        "score": score,
         "expiration_date": expiration_date,
-        "created_at": firestore.SERVER_TIMESTAMP,
+        # "created_at": firestore.SERVER_TIMESTAMP,
     }
 
+food = create_food_item("egg", 1, 3, 5, 7, "20240827")
+print(db.add_food_item(food))
 
-file = genai.upload_to_gemini("food.jpg", "fridge_food", mime_type="image/jpeg")
-if (genai.verify_upload(file.name)):
-  response = genai.send_message(file)
-  print(response)
-  genai.delete_upload(file.name)
+# genai
+# file = genai.upload_to_gemini("food.jpg", "fridge_food", mime_type="image/jpeg")
+# if (genai.verify_upload(file.name)):
+#   response = genai.send_message(file)
+#   print(response.text)
+#   genai.delete_upload(file.name)
